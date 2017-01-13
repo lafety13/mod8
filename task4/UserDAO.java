83a08802fcbf63594fb3cb27ae0f6f12fed02753
vadim <koozyy@bigmir.net>
2017-01-13 18:33:57 +0200

    updated task4

--- task4/UserDAO.java
+++ task4/UserDAO.java
@@ -2,6 +2,41 @@ package task4;
 
 import task3.AbstractDAOImpl;
 
-public class UserDAO<T> extends AbstractDAOImpl<T> {
+import java.util.List;
 
+public class UserDAO<T> extends AbstractDAOImpl<T> {
+    @Override
+    public T save(T item) {
+        return super.save(item);
+    }
+
+    @Override
+    public void delete(T item) {
+        super.delete(item);
+    }
+
+    @Override
+    public void deleteAll(List<T> item) {
+        super.deleteAll(item);
+    }
+
+    @Override
+    public void saveAll(List<T> item) {
+        super.saveAll(item);
+    }
+
+    @Override
+    public List<T> getList() {
+        return super.getList();
+    }
+
+    @Override
+    public void deleteByld(long id) {
+        super.deleteByld(id);
+    }
+
+    @Override
+    public T get(long id) {
+        return super.get(id);
+    }
 }
